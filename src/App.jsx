e22607ab90cf61180c27edff134d38196022cb10
krazytofayel <tofayel@krazyit.com>
2024-06-09 11:52:14 +0600

    contactus form design complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Programs from './Pages/Programs/Programs';
 import Aboutus from './Pages/Aboutus/Aboutus';
+import ContactUs from './Pages/ContactUs/ContactUs';
 
 
 
@@ -14,13 +15,13 @@ const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path='/programs' element={<Programs/>}/>
-        <Route path='/aboutus' element={<Aboutus/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/programs" element={<Programs />} />
+        <Route path="/aboutus" element={<Aboutus />} />
+        <Route path="/contactus" element={<ContactUs/>} />
       </Routes>
-
     </>
-  )
+  );
 }
 
 export default App
